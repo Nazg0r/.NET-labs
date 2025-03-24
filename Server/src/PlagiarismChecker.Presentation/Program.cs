0efdd629814960cf417d1978428cec6ae2d77afb
Nazg0r <gorchinshiy83@gmail.com>
2025-03-24 12:48:40 +0200

    feat: connect DataContext to system

--- Server/src/PlagiarismChecker.Presentation/Program.cs
+++ Server/src/PlagiarismChecker.Presentation/Program.cs
@@ -1,4 +1,13 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Database");
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+	opt.UseNpgsql(connectionString);
+}, ServiceLifetime.Singleton);
 
 var app = builder.Build();
 
