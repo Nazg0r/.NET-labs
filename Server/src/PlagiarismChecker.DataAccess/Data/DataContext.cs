108dbdd248d0e7b7a5a3355eeffa5f4591281abf
Nazg0r <gorchinshiy83@gmail.com>
2025-03-24 12:44:48 +0200

    feat: create DataContext

---
+++ Server/src/PlagiarismChecker.DataAccess/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccess.Data
+{
+	public class DataContext(DbContextOptions opt) : DbContext(opt)
+	{
+		public DbSet<Student> Students { get; set; }
+		public DbSet<StudentWork> StudentWorks{ get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Student>()
+				.HasMany(w => w.Works)
+				.WithOne(s => s.Student)
+				.OnDelete(DeleteBehavior.Cascade);
+		}
+	}
+}
--- Server/src/PlagiarismChecker.DataAccess/GlobalUsing.cs
+++ Server/src/PlagiarismChecker.DataAccess/GlobalUsing.cs
@@ -1,2 +1,3 @@
 ﻿global using DataAccess.Entities;
+global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations.Schema;
