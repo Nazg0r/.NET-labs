78cf8baac42390659dd9d106f7635c9b3a576213
Nazg0r <gorchinshiy83@gmail.com>
2025-04-02 22:12:36 +0300

    feat: implement mapper for entities and DTOs

---
+++ Server/src/PlagiarismChecker.BusinessLogic/Mappers/DtoToEntity.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Entities;
+using Shared.DTOs;
+
+namespace BusinessLogic.Mappers
+{
+	public static class DtoToEntity
+	{
+		public static StudentWork ToEntity(this StudentWorkRequestDto source) =>
+			new StudentWork
+			{
+				FileName = source.FileName,
+				Extension = source.Extension,
+				Content = source.Content,
+				StudentId = source.StudentId,
+				LoadDate = DateTime.UtcNow,
+			};
+
+		public static Student ToEntity(this RegistrationRequestDto source) =>
+			new Student
+			{ 
+				UserName = source.Username,
+				Name = source.Name,
+				Surname = source.Surname,
+				Group = source.Group,
+			};
+
+	}
+}
---
+++ Server/src/PlagiarismChecker.BusinessLogic/Mappers/EntityToDto.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Entities;
+using Shared.DTOs;
+
+namespace BusinessLogic.Mappers
+{
+	public static class EntityToDto
+	{
+		public static StudentWorkResponseDto ToDto(this StudentWork source) =>
+			new StudentWorkResponseDto
+			{
+				Id = source.Id,
+				Content = source.Content,
+				FileName = source.FileName + source.Extension,
+				LoadDate = source.LoadDate,
+				StudentId = source.StudentId
+			};
+	}
+}
