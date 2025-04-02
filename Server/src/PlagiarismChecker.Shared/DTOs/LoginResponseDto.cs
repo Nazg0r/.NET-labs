d2d1f86f580376a3ea764bb4f9761ab0204ba0ea
Nazg0r <gorchinshiy83@gmail.com>
2025-04-02 22:09:02 +0300

    feat: add Login and Register DTOs

---
+++ Server/src/PlagiarismChecker.Shared/DTOs/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Shared.DTOs
+{
+	public class LoginResponseDto
+	{
+		public string Token { get; set; } = default!;
+		public DateTime ExpiresDate { get; set; }
+	}
+}
