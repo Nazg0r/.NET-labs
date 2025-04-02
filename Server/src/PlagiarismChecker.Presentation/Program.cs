ff4060fe25d50a03a2e07c751644d8996e775b48
Nazg0r <gorchinshiy83@gmail.com>
2025-04-02 22:27:14 +0300

    feat: include all auth and student related services

--- Server/src/PlagiarismChecker.Presentation/Program.cs
+++ Server/src/PlagiarismChecker.Presentation/Program.cs
@@ -1,5 +1,6 @@
 using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using Presentation.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Database");
@@ -9,6 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 	opt.UseNpgsql(connectionString);
 }, ServiceLifetime.Singleton);
 
+builder.Services.AddRepositories();
+builder.Services.AddCustomAuthentification(builder.Configuration);
+builder.Services.AddServices();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
+
+app.UseAuthentication();
 
 app.Run();
