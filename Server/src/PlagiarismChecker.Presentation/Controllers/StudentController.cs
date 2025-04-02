c496f75b1b766a883f4e53584a6660475d9ac687
Nazg0r <gorchinshiy83@gmail.com>
2025-04-02 22:25:35 +0300

    feat: create Student controller

---
+++ Server/src/PlagiarismChecker.Presentation/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+
+namespace Presentation.Controllers
+{
+	public class StudentController(IAuthentificationService authentificationService)
+		: BaseController
+	{
+		[HttpPost("register")]
+		public async Task<ActionResult> Register(RegistrationRequestDto request)
+		{
+			await authentificationService.RegisterAsync(request);
+
+			return Created();
+		}
+
+		[HttpPost("login")]
+		public async Task<ActionResult> Login(LoginRequestDto request)
+		{
+			var response = await authentificationService.LoginAsync(request);
+
+			return Ok(response);
+		}
+	}
+}
