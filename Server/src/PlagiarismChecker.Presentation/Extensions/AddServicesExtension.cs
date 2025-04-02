928056a6caa9c1c79c8aea059a2a926aaf1632ec
Nazg0r <gorchinshiy83@gmail.com>
2025-04-02 22:15:48 +0300

    feat: create AddServicesExtension

---
+++ Server/src/PlagiarismChecker.Presentation/Extensions/AddServicesExtension.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+
+namespace Presentation.Extensions
+{
+	public static class AddServicesExtension
+	{
+		public static IServiceCollection AddServices(this IServiceCollection services)
+		{
+			services.AddScoped<IAuthentificationService, AuthentificationService>();
+			return services;
+		}
+	}
+}
