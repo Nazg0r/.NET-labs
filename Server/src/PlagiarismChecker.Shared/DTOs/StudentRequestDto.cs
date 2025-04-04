8a8aa27bef5fab30b7efe8b72aa7fd43dfc10778
Nazg0r <gorchinshiy83@gmail.com>
2025-04-04 16:01:46 +0300

    feat: add new student and student work DTOs

---
+++ Server/src/PlagiarismChecker.Shared/DTOs/StudentRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.DTOs
+{
+	public class StudentRequestDto
+	{
+		public string Username { get; set; } = default!;
+	}
+}
---
+++ Server/src/PlagiarismChecker.Shared/DTOs/StudentWorkDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.DTOs
+{
+	public class StudentWorkDto
+	{
+		public Guid Id { get; set; } = default!;
+		public string FileName { get; set; } = default!;
+		public DateTime LoadDate { get; set; } = default!;
+		//[JsonIgnore]
+		public byte[]? Content { get; set; }
+	}
+}
