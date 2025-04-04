838b2aab98e11cc0a321f5b716bfc8a36f8c8ad1
Nazg0r <gorchinshiy83@gmail.com>
2025-04-04 15:58:56 +0300

    feat: implement StudentWorkRepository

---
+++ Server/src/PlagiarismChecker.DataAccess/Interfaces/IStudentWorkRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccess.Interfaces
+{
+	public interface IStudentWorkRepository
+	{
+		Task<StudentWork?> GetWorkByIdAsync(Guid id);
+		Task<List<StudentWork>> GetAllWorksAsync();
+		Task<StudentWork> AddNewWorkAsync(StudentWork work);
+		Task<bool> DeleteWorkAsync(Guid id);
+	}
+}
