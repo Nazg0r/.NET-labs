6c66f50e792980f530440268a67da0223509af27
Nazg0r <gorchinshiy83@gmail.com>
2025-04-04 16:08:25 +0300

    feat: add StudentWorkRepository to CI container

---
+++ Server/src/PlagiarismChecker.Presentation/Extensions/AddRepositoriesExtension.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Repositories;
+
+namespace Presentation.Extensions
+{
+	public static class AddRepositoriesExtension
+	{
+		public static IServiceCollection AddRepositories(this IServiceCollection services) 
+		{
+			services.AddSingleton<IStudentWorkRepository, StudentWorkRepository>();
+			return services;
+		}
+	}
+}
