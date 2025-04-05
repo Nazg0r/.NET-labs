4aa4c9c974452e9efd428dca9e3d290684aafd97
Nazg0r <gorchinshiy83@gmail.com>
2025-04-05 22:34:07 +0300

    feat: create and include global error handler

--- Server/src/PlagiarismChecker.Presentation/Program.cs
+++ Server/src/PlagiarismChecker.Presentation/Program.cs
@@ -2,6 +2,7 @@ using DataAccess.Data;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Presentation.Extensions;
+using Shared.Errors.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Database");
@@ -16,11 +17,14 @@ builder.Services.AddControllers();
 builder.Services.AddServices();
 builder.Services.AddRepositories();
 builder.Services.AddCustomAuthentification(builder.Configuration);
+builder.Services.AddExceptionHandler<CustomErrorHandler>();
 
 var app = builder.Build();
 
 app.MapControllers();
 
 app.UseAuthentication();
+
+app.UseExceptionHandler(opt => { });
 
 app.Run();
---
+++ Server/src/PlagiarismChecker.Shared/Errors/Handler/CustomErrorHandler.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shared.Errors.Handler
+{
+	public class CustomErrorHandler : IExceptionHandler
+	{
+		public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+		{
+			ProblemDetails problemDetails = new()
+			{
+				Detail = exception.Message,
+				Title = exception.GetType().Name,
+				Instance = context.Request.Path
+			};
+
+			problemDetails.Status = exception switch
+			{
+				NotFoundException => StatusCodes.Status404NotFound,
+				CreationException => StatusCodes.Status400BadRequest,
+				ValidationException => StatusCodes.Status400BadRequest,
+				ArgumentException => StatusCodes.Status400BadRequest,
+				UnauthorizedException => StatusCodes.Status401Unauthorized,
+				_ => StatusCodes.Status500InternalServerError
+			};
+
+			if (exception is ValidationException validationException)
+				problemDetails.Extensions.Add("ValidationException", validationException.Errors);
+
+			context.Response.StatusCode = problemDetails.Status??StatusCodes.Status500InternalServerError;
+
+			await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+
+
+			return true;
+		}
+	}
+}
