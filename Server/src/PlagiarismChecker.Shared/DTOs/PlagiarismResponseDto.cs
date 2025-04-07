09863a2dbba26cc3e78c82b1c72c916b24eb098b
Nazg0r <gorchinshiy83@gmail.com>
2025-04-07 20:29:43 +0300

    feat: add PlagiarismResponseDto

---
+++ Server/src/PlagiarismChecker.Shared/DTOs/PlagiarismResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DTOs
+{
+	public class PlagiarismResponseDto
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; } = default!;
+		public double SimilarityPercentage { get; set; } = default!;
+	}
+}
