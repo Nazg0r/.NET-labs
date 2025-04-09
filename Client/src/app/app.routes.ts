1e742b3e8c1c1fe14e8ac40ec6a8a9d364e53bb7
Nazg0r <gorchinshiy83@gmail.com>
2025-04-09 15:29:09 +0300

    feat: init angular client

---
+++ Client/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {RegistrationComponent} from "./src/pages/registration/registration.component";
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegistrationComponent
+  }
+];
