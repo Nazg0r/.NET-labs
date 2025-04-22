3d4f4c79a94a6efd9c5c93ec7ff3c81925915576
Nazg0r <gorchinshiy83@gmail.com>
2025-04-22 21:53:28 +0300

    feat: write routing and include components to app

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './src/pages/welcome/welcome.component';
+import { HeaderComponent } from './src/pages/header/header.component';
+import { RegistrationComponent } from './src/pages/authentification/registration/registration.component';
+import { BackgroundComponent } from './src/pages/background/background.component';
+import { LoginComponent } from './src/pages/authentification/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    WelcomeComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    BackgroundComponent,
+    LoginComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
-import {RegistrationComponent} from "./src/pages/registration/registration.component";
+import { RegistrationComponent } from './src/pages/authentification/registration/registration.component';
+import { WelcomeComponent } from './src/pages/welcome/welcome.component';
+import { LoginComponent } from './src/pages/authentification/login/login.component';
+import { WorkspaceComponent } from './src/pages/workspace/workspace.component';
+import { AuthGuardService } from './src/pages/authentification/AuthGuard.service';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
     path: 'register',
-    component: RegistrationComponent
-  }
+    component: RegistrationComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'workspace',
+    component: WorkspaceComponent,
+    canActivate: [AuthGuardService],
+  },
 ];
