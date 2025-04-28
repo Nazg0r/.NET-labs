4fdfa8a1d03db70b2e34174b80abcac4c7b7fc91
Nazg0r <gorchinshiy83@gmail.com>
2025-04-28 19:16:46 +0300

    upd: refined and include created components

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './src/pages/welcome/welcome.component';
 import { HeaderComponent } from './src/pages/header/header.component';
 import { RegistrationComponent } from './src/pages/authentification/registration/registration.component';
 import { BackgroundComponent } from './src/pages/background/background.component';
 import { LoginComponent } from './src/pages/authentification/login/login.component';
+import { NotificationService } from './src/pages/notifications/notification.service';
+import { NotificationsComponent } from './src/pages/notifications/notifications.component';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +18,11 @@ import { LoginComponent } from './src/pages/authentification/login/login.compone
     RegistrationComponent,
     BackgroundComponent,
     LoginComponent,
+    NotificationsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  notificationService = inject(NotificationService);
+}
