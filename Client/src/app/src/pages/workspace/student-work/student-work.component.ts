e820260cb3c65b8b0cbbe8c15d80bc15a8fc9dd9
Nazg0r <gorchinshiy83@gmail.com>
2025-04-29 21:45:05 +0300

    feat: incluede details dialog and do small improvements

--- Client/src/app/src/pages/workspace/student-work/student-work.component.ts
+++ Client/src/app/src/pages/workspace/student-work/student-work.component.ts
@@ -3,7 +3,6 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-student-work',
   standalone: true,
-  imports: [],
   templateUrl: './student-work.component.html',
   styleUrl: './student-work.component.css',
 })
--- Client/src/app/src/pages/workspace/workspace.component.ts
+++ Client/src/app/src/pages/workspace/workspace.component.ts
@@ -12,19 +12,22 @@ import { StudentWorkComponent } from './student-work/student-work.component';
 import { HttpClient, HttpEventType } from '@angular/common/http';
 import { NotificationService } from '../notifications/notification.service';
 import { FormsModule } from '@angular/forms';
+import { DetailsComponent } from './student-work/details/details.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [StudentWorkComponent, FormsModule],
+  imports: [StudentWorkComponent, FormsModule, DetailsComponent],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.css',
 })
 export class WorkspaceComponent implements OnInit {
   private notificationService = inject(NotificationService);
   private httpClient = inject(HttpClient);
-  studentInfoService = inject(StudentInfoService);
+  private studentInfoService = inject(StudentInfoService);
   private destroyRef = inject(DestroyRef);
+  selectedFile = signal<StudentWork | undefined>(undefined);
+  isModalOpen = signal<boolean>(false);
   searchReq = signal('');
   studentWorks = computed(() => this.studentInfoService.student()?.works);
   filteredWorks = computed(() => {
@@ -78,5 +81,14 @@ export class WorkspaceComponent implements OnInit {
       this.destroyRef.onDestroy(() => subscription.unsubscribe());
     }
     return;
+  }
+
+  onFileDblClick(work: StudentWork) {
+    this.selectedFile.set(work);
+    this.isModalOpen.set(true);
+  }
+
+  onModalClosed() {
+    this.isModalOpen.set(false);
   }
 }
