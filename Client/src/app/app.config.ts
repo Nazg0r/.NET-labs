1c7c4a0df7fce4d2efd9a39150e899b425a8c687
Nazg0r <gorchinshiy83@gmail.com>
2025-04-30 12:48:02 +0300

    fix: solve problem with JWTInterceptorService

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -2,14 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { JWTInterceptorService } from './src/pages/authentification/JWTInterceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: JWTInterceptorService,
