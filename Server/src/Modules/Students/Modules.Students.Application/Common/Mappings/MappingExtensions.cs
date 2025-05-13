6ba015db97e3e71cec26fbe2c4627734955e289c
Nazg0r <gorchinshiy83@gmail.com>
2025-05-13 21:03:16 +0300

    feat: implement extension methods for models mapping

---
+++ Server/src/Modules/Students/Modules.Students.Application/Common/Mappings/MappingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Modules.Students.Application.Common.Models;
+using Modules.Students.Domain.Entities;
+
+namespace Modules.Students.Application.Common.Mappings
+{
+	public static class MappingExtensions
+	{
+		public static Student ToEntity(this RegisterStudentCommand source) =>
+			new Student
+			{
+				Username = source.Username,
+				Name = source.Name,
+				Surname = source.Surname,
+				Group = source.Group,
+			};
+
+		public static GetStudentByUsernameResponse ToDto(this Student source) =>
+			new GetStudentByUsernameResponse
+			{
+				Id = source.Id.ToString(),
+				Username = source.Username!,
+				Name = source.Name,
+				Surname = source.Surname,
+				Group = source.Group,
+			};
+	}
+}
