f71c0559211276aa5e19f95c5f793ebcb3e6d111
Nazg0r <gorchinshiy83@gmail.com>
2025-05-14 15:52:12 +0300

    feat: add works module DIs to webapi project

--- Server/src/Application/Aggregator/API/Program.cs
+++ Server/src/Application/Aggregator/API/Program.cs
@@ -2,6 +2,8 @@ using API.Endpoints;
 using Modules.Students.Application;
 using Modules.Students.Infrastructure;
 using Modules.Students.Persistence;
+using Modules.Works.Application;
+using Modules.Works.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,13 @@ builder.AddStudentModulePersistence();
 builder.AddStudentModuleInfrastructure();
 builder.AddStudentModuleApplication();
 
+builder.AddWorksModuleApplication();
+builder.AddWorksModulePersistence();
+
 var app = builder.Build();
 
 app.MapStudentEndpoints();
+app.MapWorkEndpoints();
 
 app.UseAuthentication();
 app.UseAuthorization();
