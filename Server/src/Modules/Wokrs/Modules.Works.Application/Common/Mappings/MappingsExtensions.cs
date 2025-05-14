c217034429f2e41004005d2d134606ed6e44b59b
Nazg0r <gorchinshiy83@gmail.com>
2025-05-14 15:37:09 +0300

    feat: implement extension methods for work models mapping

---
+++ Server/src/Modules/Wokrs/Modules.Works.Application/Common/Mappings/MappingsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Modules.Works.Application.Common.Models;
+
+namespace Modules.Works.Application.Common.Mappings
+{
+	public static class MappingsExtensions
+	{
+		public static ProcessedWorkResponse ToDto(this Work source) =>
+			new ProcessedWorkResponse
+			{
+				Id = source.Id,
+				Content = source.Content,
+				FileName = source.FileName + source.Extension,
+				LoadDate = source.LoadDate,
+				StudentId = source.StudentId
+			};
+	}
+}
