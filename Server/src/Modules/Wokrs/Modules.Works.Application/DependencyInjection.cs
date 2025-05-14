aa18860d781d33a08016eeee9bf89041d8286d88
Nazg0r <gorchinshiy83@gmail.com>
2025-05-14 15:40:59 +0300

    feat: register dependences injection from work module Application layer

---
+++ Server/src/Modules/Wokrs/Modules.Works.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Modules.Works.Application.UseCases.DeleteWork;
+using Modules.Works.Application.UseCases.GetAllWorks;
+using Modules.Works.Application.UseCases.GetSimilarityPercentage;
+using Modules.Works.Application.UseCases.GetWorkById;
+using Modules.Works.Application.UseCases.UploadWork;
+
+namespace Modules.Works.Application
+{
+	public static class DependencyInjection
+	{
+		public static void AddWorksModuleApplication(this IHostApplicationBuilder builder)
+		{
+			builder.Services
+				.AddUploadWork()
+				.AddGetWorkById()
+				.AddGetSimilarityPercentage()
+				.AddGetAllWorks()
+				.AddDeleteWork();
+		}
+	}
+}
