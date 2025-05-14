d9c0566e1f6d7d7d217f406d725e378ee1c96dff
Nazg0r <gorchinshiy83@gmail.com>
2025-05-14 15:46:06 +0300

    feat: implement repositories for work entity

---
+++ Server/src/Modules/Wokrs/Modules.Works.Persistence/Repositories/WorkRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modules.Works.Application.Contracts;
+using Modules.Works.Domain.Entities;
+using Modules.Works.Persistence.Data;
+
+namespace Modules.Works.Persistence.Repositories
+{
+	public class WorkRepository(WorkDbContext context) : IWorkRepository
+	{
+		public async Task<Work?> GetWorkByIdAsync(Guid id) =>
+			await context.Works.FindAsync(id);
+
+		public async Task<List<Work>> GetAllWorksAsync() =>
+			await context.Works.ToListAsync();
+
+		public async Task<Work> AddNewWorkAsync(Work work)
+		{
+			await context.Works.AddAsync(work);
+			await context.SaveChangesAsync();
+
+			return work;
+		}
+
+		public async Task<bool> DeleteWorkAsync(Guid id)
+		{
+			var studentWork = await context.Works.FindAsync(id);
+
+			if (studentWork is null) return false;
+
+			context.Remove(studentWork);
+			await context.SaveChangesAsync();
+
+			return true;
+		}
+	}
+}
