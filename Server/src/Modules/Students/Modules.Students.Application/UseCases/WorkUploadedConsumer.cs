10df8705c056c740f49b514ec8d260fc698a900d
Nazg0r <gorchinshiy83@gmail.com>
2025-05-15 16:59:01 +0300

    feat: create WorkUploadedConsumer to process notifications when new work uploaded

---
+++ Server/src/Modules/Students/Modules.Students.Application/UseCases/WorkUploadedConsumer.cs
@@ -0,0 +1,12 @@
+﻿using MassTransit;
+using Modules.Works.IntegrationEvents;
+
+namespace Modules.Students.Application.UseCases
+{
+	public class WorkUploadedConsumer(IStudentCommands studentCommands)
+		: IConsumer<WorkUploadedEvent>
+	{
+		public async Task Consume(ConsumeContext<WorkUploadedEvent> context) =>
+			await studentCommands.AddWorkIdToStudent(context.Message.studentId, context.Message.workId);
+	}
+}
