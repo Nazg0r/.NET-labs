36f4a522aa9cef0f37bf3766cb0d5ca23a4fe895
Nazg0r <gorchinshiy83@gmail.com>
2025-05-16 23:36:17 +0300

    feat: write unit tests for mapping methods in Works Application layer

---
+++ Server/tests/Modules/Works/Application.Tests/MappingsTests.cs
@@ -0,0 +1,46 @@
+﻿using Modules.Works.Application.Common.Mappings;
+using Modules.Works.Application.Common.Models;
+using Modules.Works.IntegrationEvents;
+using TestsTools;
+
+namespace Application.Tests;
+
+public class MappingsTests
+{
+	[Fact]
+	public void Should_ReturnProcessedWorkResponse_WhenCalledFromWork()
+	{
+		// Arrange
+		var work = SharedTestsData.WorksWithDifferentExtension.First();
+
+		// Act
+		var result = work.ToDto();
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.IsType<ProcessedWorkResponse>(result);
+		Assert.Equal(work.Id, result.Id);
+		Assert.Equal(work.FileName + work.Extension, result.FileName);
+		Assert.Equal(work.LoadDate, result.LoadDate);
+		Assert.Equal(work.Content, result.Content);
+		Assert.Equal(work.StudentId, result.StudentId);
+	}
+
+	[Fact]
+	public void Should_ReturnStudentWorkDto_WhenCalledFromWork()
+	{
+		// Arrange
+		var work = SharedTestsData.WorksWithDifferentExtension.First();
+
+		// Act
+		var result = work.ToEventResponse();
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.IsType<StudentWorkDto>(result);
+		Assert.Equal(work.Id, result.Id);
+		Assert.Equal(work.FileName + work.Extension, result.FileName);
+		Assert.Equal(work.LoadDate, result.LoadDate);
+		Assert.Equal(work.Content, result.Content);
+	}
+}
