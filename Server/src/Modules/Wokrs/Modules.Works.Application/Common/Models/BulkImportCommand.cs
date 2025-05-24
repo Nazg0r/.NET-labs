e5b3a1d1c97625ddb5851bd12b35782c900b244d
Nazg0r <gorchinshiy83@gmail.com>
2025-05-24 20:41:03 +0300

    feat: create BulkImportCommand

---
+++ Server/src/Modules/Wokrs/Modules.Works.Application/Common/Models/BulkImportCommand.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Contracts;
+using Microsoft.AspNetCore.Http;
+
+namespace Modules.Works.Application.Common.Models
+{
+	public class BulkImportCommand : ICommand
+	{
+		public Guid Id { get; } = Guid.Empty!;
+		public IFormFile File { get; set; } = null!;
+	}
+}
