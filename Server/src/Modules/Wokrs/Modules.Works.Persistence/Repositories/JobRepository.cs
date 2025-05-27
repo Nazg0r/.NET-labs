a1e939df5cbd614dd96af6975259269a3edb7312
Nazg0r <gorchinshiy83@gmail.com>
2025-05-27 16:09:13 +0300

    feat: implement JobRepository

---
+++ Server/src/Modules/Wokrs/Modules.Works.Persistence/Repositories/JobRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modules.Works.Application.Contracts;
+using Modules.Works.Domain.Entities;
+using Modules.Works.Persistence.Data;
+
+namespace Modules.Works.Persistence.Repositories
+{
+	public class JobRepository(WorkDbContext context) : IJobRepository
+	{
+		public async Task<bool> StoreJobResultAsync(UploadFIleJobResult jobResult)
+		{
+			await context.UploadFIleJobResults.AddAsync(jobResult);
+			var affectedRows = await context.SaveChangesAsync();
+
+			return affectedRows > 0;
+		}
+
+		public async Task<UploadFIleJobResult?> GetJobResultByJobIdAsync(string jobId) =>
+			await context.UploadFIleJobResults
+				.FirstOrDefaultAsync(r => r.JobId == jobId);
+	}
+}
