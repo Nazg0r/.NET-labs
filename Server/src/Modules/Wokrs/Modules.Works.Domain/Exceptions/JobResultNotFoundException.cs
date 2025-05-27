60c0e74f41089ea7d032e02973859d99cde997a1
Nazg0r <gorchinshiy83@gmail.com>
2025-05-27 17:13:29 +0300

    feat: create JobResultNotFoundException

---
+++ Server/src/Modules/Wokrs/Modules.Works.Domain/Exceptions/JobResultNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Modules.Works.Domain.Exceptions
+{
+	public class JobResultNotFoundException(string jobId)
+		: NotFoundException($"JobResult with jobId: \"{jobId}\"");
+}
