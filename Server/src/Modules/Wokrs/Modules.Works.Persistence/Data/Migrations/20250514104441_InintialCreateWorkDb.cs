cb6c8e14bf721582a711a3f94eaa8901776eaea0
Nazg0r <gorchinshiy83@gmail.com>
2025-05-29 14:46:36 +0300

    feat: add project migrations

---
+++ Server/src/Modules/Wokrs/Modules.Works.Persistence/Data/Migrations/20250514104441_InintialCreateWorkDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Works.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InintialCreateWorkDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "works");
+
+            migrationBuilder.CreateTable(
+                name: "Works",
+                schema: "works",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    file_name = table.Column<string>(type: "text", nullable: false),
+                    load_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    extension = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<byte[]>(type: "bytea", nullable: false),
+                    student_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Works", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Works",
+                schema: "works");
+        }
+    }
+}
---
+++ Server/src/Modules/Wokrs/Modules.Works.Persistence/Data/Migrations/20250527134335_AddUploadFIleJobResultsEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Works.Persistence.src.Modules.Wokrs.Modules.Works.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUploadFIleJobResultsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UploadFIleJobResults",
+                schema: "works",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    job_id = table.Column<string>(type: "text", nullable: true),
+                    work_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UploadFIleJobResults", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UploadFIleJobResults",
+                schema: "works");
+        }
+    }
+}
